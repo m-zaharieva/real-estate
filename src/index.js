dace5976c849579075c127f59c0959a2a62ae0b1
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-24 11:26:48 +0300

    Initialize Databasa

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { initHandlebars } = require('./config/expressHandlebars.js');
+const { initDatabase } = require('./config/initDatabase.js');
+const path = require('path');
+
+// Local Modules
+const config = require('./config/config.json')[process.env.NODE_ENV];
+
+
+// Init ExporessJS
+const app = express();
+// Init handlebars
+initHandlebars(app);
+// Static files 
+
+
+
+// Init Database and app port listening
+initDatabase(config.DB_CONNECTION_STRING)
+    .then(() => {
+        app.listen(config.PORT, () => {
+            console.log(`App is running on http://localhost:${config.PORT} ...`);
+    });
+
+});
