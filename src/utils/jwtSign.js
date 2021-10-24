974bb7c6cf60a4f3915dca280007ac0c71b8bf2b
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-24 14:49:03 +0300

    convert jsonwebtoken verify function to a promise function

--- src/utils/jwtSign.js
+++ src/utils/jwtSign.js
@@ -13,3 +13,17 @@ exports.jwtSign = function (payload, secret) {
 
     return promise;
 }
+
+exports.jwtVerify = function(token, secret) {
+    let promise = new Promise((resolve, reject) => {
+        jwt.verify(token, secret, function(err, decodedToken) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(decodedToken);
+            }
+        });
+    });
+
+    return promise;
+}
