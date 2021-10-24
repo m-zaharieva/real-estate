7740936c200a90e3bbbeec8848f3b0adb948ca8e
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-24 15:01:39 +0300

    get create house page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController.js');
 const authController = require('./controllers/authController.js');
+const housingController = require('./controllers/housingController.js');
 
 
 router.use(homeController);
 router.use('/auth', authController);
+router.use('/housing', housingController);
 
 
 
